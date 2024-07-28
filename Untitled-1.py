ebdb50893b0630b9e5f8ace186671b1c37fd77d1
mahan <112346652+mahan-coder@users.noreply.github.com>
2024-07-28 22:39:59 +0330

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Original array
+a = np.array([[1, 2, 3], [4, 5, 6]])
+
+# Create an array of zeros with the same shape as 'a'
+zero_array = np.zeros_like(a)
+
+print(zero_array)
